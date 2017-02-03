d0c6a9f2118251373c221263289024c825dcce9f
Willian Ribeiro Angelo <agfoccus@gmail.com>
2017-02-03 20:16:12 -0200

    Fix Push Configuration

--- cloud/main.js
+++ cloud/main.js
@@ -11,8 +11,8 @@ const Dashboard       = require('./class/Dashboard');
 const Push            = require('./class/Push');
 
 // Parse Push Android
-let PUSH_ANDROID_SENDER  = process.env.PUSH_ANDROID_SENDER;
-let PUSH_ANDROID_API_KEY = process.env.PUSH_ANDROID_API_KEY;
+let FIREBASE_SENDER_KEY  = process.env.FIREBASE_SENDER_KEY;
+let FIREBASE_API_KEY     = process.env.FIREBASE_API_KEY;
 
 
 // Push
@@ -33,7 +33,7 @@ Parse.Cloud.define('createChatChannel', ChatChannel.createChatChannel);
 Parse.Cloud.define('createMessage', ChatMessage.createMessage);
 Parse.Cloud.define('getChatMessages', ChatMessage.getMessages);
 // If the push is set
-if (PUSH_ANDROID_API_KEY && PUSH_ANDROID_SENDER) {
+if (FIREBASE_API_KEY && FIREBASE_SENDER_KEY) {
   Parse.Cloud.afterSave('ChatMessage', ChatMessage.afterSave);
 }
 
@@ -45,7 +45,7 @@ Parse.Cloud.define('dashboard', Dashboard.home);
 Parse.Cloud.define('feedActivity', GalleryActivity.feed);
 
 // If the push is set
-if (PUSH_ANDROID_API_KEY && PUSH_ANDROID_SENDER) {
+if (FIREBASE_API_KEY && FIREBASE_SENDER_KEY) {
   Parse.Cloud.afterSave('GalleryActivity', GalleryActivity.afterSave);
 }
 
